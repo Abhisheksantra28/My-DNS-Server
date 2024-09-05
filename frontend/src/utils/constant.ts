34afd63f97078dbba69d8e38afb497b8c1f5c8ce
Abhishek Santra <abhisheksantra28@gmail.com>
2024-09-05 12:41:38 +0530

    Update SERVER_URL constant

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -1,2 +1,3 @@
 // export const SERVER_URL = 'http://localhost:3000';
-export const SERVER_URL = 'http://13.126.1.202:5000';
+// export const SERVER_URL = 'http://13.126.1.202:5000';
+export const SERVER_URL = 'ns1.abhisheksantra.tech:5000';
